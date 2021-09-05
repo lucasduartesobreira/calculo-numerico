8a305d92c4718ccf4de56e2fce8b362b728ad51b
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-09-04 22:32:36 -0300

    feat(run): Added all functions

--- run.py
+++ run.py
@@ -1,17 +1,47 @@
-from math import cos, e
+from math import cos, e, sin, log10
 import bisection
 
 
-def f(x):
+def ex_18(x):
     return e**(-x**2) - cos(x)
 
 
-def run_bisection():
-    limite_inicial = float(input('Limite inicial: '))
-    limite_final = float(input('Limite final: '))
-    tol = float(input('Tol: '))
-
-    bisection.bisection(f, limite_inicial, limite_final, tol)
+def ex_19(x):
+    return x**3 - x - 1
 
 
-run_bisection()
+def ex_20(x):
+    return 4*sin(x) - e**x
+
+
+def ex_21(x):
+    return x*log10(x) - 1
+
+
+def test_bisection():
+    limite_inicial = 1
+    limite_final = 2
+    tol = 1e-4
+
+    bisection.bisection(ex_18, limite_inicial, limite_final, tol)
+
+    limite_inicial = 1
+    limite_final = 2
+    tol = 1e-6
+
+    bisection.bisection(ex_19, limite_inicial, limite_final, tol)
+
+    limite_inicial = 0
+    limite_final = 1
+    tol = 1e-5
+
+    bisection.bisection(ex_20, limite_inicial, limite_final, tol)
+
+    limite_inicial = 2
+    limite_final = 3
+    tol = 1e-7
+
+    bisection.bisection(ex_21, limite_inicial, limite_final, tol)
+
+
+test_bisection()
